669f273ee1a287fac30bbdec1d9a76fe7cdde04c
Michael Rubino <parkcathedralgrace@gmail.com>
2019-05-09 17:27:49 -0400

    Vanilla JS-Booklist

--- Vanilla JS/booklist.js
+++ Vanilla JS/booklist.js
@@ -14,9 +14,22 @@ document.addEventListener('DOMContentLoaded',()=>{
     let list = document.createElement('ul');
 
     for(let i=0;i<books.length;i++){
-        let bookTitle = books[i].title;
-        let bookAuthor = books[i].author;
-        let alreadyRead = books[i].alreadyRead;
-        console.log(bookTitle,bookAuthor,alreadyRead);
+        let bookTitle = document.createElement('li');
+        bookTitle.innerHTML = books[i].title;
+        let bookDetails = document.createElement('ul');
+        let bookAuthor = document.createElement('li');
+        bookAuthor.innerHTML = books[i].author;
+        let alreadyRead = document.createElement('li');
+        alreadyRead.innerHTML = books[i].alreadyRead;
+        //console.log(bookTitle,bookAuthor,alreadyRead);
+
+        let bookItem = document.createElement('p');
+        bookDetails.appendChild(bookAuthor);
+        bookDetails.appendChild(alreadyRead);
+        bookTitle.appendChild(bookDetails);
+        bookItem.appendChild(bookTitle);
+        list.appendChild(bookItem);
     }
+
+    document.body.appendChild(list);
 })
